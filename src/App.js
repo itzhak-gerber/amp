0e7817db8527252557a2e97236186e1c76f8c124
Itzhak-Gerber <gerber.itzhak@gmail.com>
2021-10-29 16:39:24 +0300

    added grid

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V3</h1>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const App = () => {
+   const rowData = [
+       {make: "Toyota", model: "Celica", price: 35000},
+       {make: "Ford", model: "Mondeo", price: 32000},
+       {make: "Porsche", model: "Boxter", price: 72000}
+   ];
+
+   return (
+       <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+           <AgGridReact
+               rowData={rowData}>
+               <AgGridColumn field="make"></AgGridColumn>
+               <AgGridColumn field="model"></AgGridColumn>
+               <AgGridColumn field="price"></AgGridColumn>
+           </AgGridReact>
+       </div>
+   );
+};
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>Hello from V3</h1>
+//       </header>
+//     </div>
+//   );
+// }
 
 
 export default App;
